aa20b229b7be6f4264114478315eadc265858527
Pankaj Soam <pankaj.soam111@gmail.com>
2021-05-16 16:42:52 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -11,20 +11,45 @@ function App() {
   const [exchangeRate,setExchangeRate] = useState();
   const [amount, setAmount] = useState(1);
   const [amountInFromCurrency, setAmountInFromCurrency] = useState(true);
-  console.log(exchangeRate);
+
+  let toAmount, fromAmount
+  if(amountInFromCurrency){
+    fromAmount = amount;
+    toAmount = amount * exchangeRate || 0
+  }else{
+    toAmount = amount
+    fromAmount = amount / exchangeRate
+  }
 
   useEffect(()=>{
     fetch(BASE_URL)
     .then(res => res.json())
     .then(data => {
       const firstCurrency = Object.keys(data.rates)[0];
-      setCurrencyOptions([...Object.keys(data.rates)]);
+      setCurrencyOptions([data.base,...Object.keys(data.rates)]);
       setFromCurrency(data.base);
       setToCurrency(firstCurrency);
       setExchangeRate(data.rates[firstCurrency])
     });
   },[])
 
+  useEffect(()=>{
+    if(fromCurrency!=null && toCurrency!=null)
+    {
+      fetch(`${BASE_URL}?base=${fromCurrency}&symbols=${toCurrency}`)
+      .then(res => res.json())
+      .then(data => setExchangeRate(data.rates[toCurrency]))
+    }
+  },[fromCurrency,toCurrency])
+
+  function handleFromAmountChange(e){
+    setAmount(e.target.value)
+    setAmountInFromCurrency(true)
+  }
+  function handleToAmountChange(e){
+    setAmount(e.target.value)
+    setAmountInFromCurrency(false)
+  }
 
   return (
     <>
@@ -33,6 +58,8 @@ function App() {
         currencyOptions={currencyOptions}
         selectCurrency={fromCurrency}        
         onChangeCurrency={e => setFromCurrency(e.target.value)}
+        onChangeAmount={handleFromAmountChange}
+        amount = {fromAmount}
       />
 
       <div className="equals">=</div>
@@ -41,6 +68,8 @@ function App() {
         currencyOptions={currencyOptions}
         selectCurrency={toCurrency}
         onChangeCurrency={e => setToCurrency(e.target.value)}
+        onChangeAmount={handleToAmountChange}
+        amount = {toAmount}
         />
     </>
   );
--- src/currencyRow.js
+++ src/currencyRow.js
@@ -4,12 +4,14 @@ export default function CurrencyRow(props){
     const{
         currencyOptions,
         selectCurrency,
-        onChangeCurrency
+        onChangeCurrency,
+        onChangeAmount,
+        amount
     } = props
     return(
         <div>
-            <input type="number" className="input"/>
-            <select value={selectCurrency} onChange={onChangeCurrency}>
+            <input type="number" className="input" value = {amount} onChange={onChangeAmount}/>
+            <select value={selectCurrency} onChange={onChangeCurrency} >
                 {currencyOptions.map(option => (
                     <option key={option} value={option}>{option}</option>
                 ))}
